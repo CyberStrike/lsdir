ce254513e72621ebd94f899581ff211c8160a67e
Chris Scott <cyberstrike@gmail.com>
2017-11-11 16:12:23 -0500

    added readme

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const dir = './sample/';
+
 fs.readdir(dir, (err, files) => {
   files.forEach( file => {
     console.log(file);
--- script.php
+++ script.php
@@ -2,7 +2,3 @@
   $dir   = './sample';
   $files = scandir($dir);
   print_r($files);
-
-  // Make Test Files
-  // mkdir sample && cd sample
-  // for i in {1..1000}; do touch ./sample/$i.txt; done
